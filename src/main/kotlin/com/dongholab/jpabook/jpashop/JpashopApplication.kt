8008e024b41ee153e6395c5cf8fc601422ef8f16
Teddy <teddy.ryan@kakaostyle.com>
2022-09-07 00:06:55 +0900

    init project

---
+++ src/main/kotlin/com/dongholab/jpabook/jpashop/JpashopApplication.kt
@@ -0,0 +1,11 @@
+package com.dongholab.jpabook.jpashop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpashopApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpashopApplication>(*args)
+}
---
+++ src/test/kotlin/com/dongholab/jpabook/jpashop/JpabookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dongholab.jpabook.jpashop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpabookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
